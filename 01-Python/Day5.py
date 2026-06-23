9c648dacbc46611433827c542e8c87ac46eedbbf
Umair <umairrajpoot9319@gmail.com>
2026-06-23 22:17:20 +0530

    Day 5 Completed Python

---
+++ 01-Python/Day5.py
@@ -0,0 +1,9 @@
+#Program-01
+# Ques : "Python", "Java", "C++", "Python", "Javascript", "Java", "Python", "Java", "C++", "C"
+#  this is given list of subject for students. assume one classroom is required for one subject. How many classroom are needed by all students.
+
+subjects = {
+    "Python", "Java", "C++", "Python", "Javascript", "Java", "Python", "Java", "C++", "C"
+}    # store data in set , then print length od set 
+print(subjects)
+print(len(subjects)) 
