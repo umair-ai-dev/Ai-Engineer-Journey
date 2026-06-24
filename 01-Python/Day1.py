490c43fbb81754d39b35ae44ce559f2aa96a4657
Umair <umairrajpoot9319@gmail.com>
2026-06-24 19:35:44 +0530

    Updated Python Day 1

--- 01-Python/Day1.py
+++ 01-Python/Day1.py
@@ -1,11 +1,17 @@
-#first program 
+#Program-01
+
+# first program 
 print("hello world!")
 
-#name and about
+#Program-02
+
+# name and about
 name= input("enter your name:")
 print(f"Hello my name is {name},this is my Ai journey.")
 
-#Data types
+#Program-03
+
+# Data types
 a=10
 b=2.0
 c= "umair"
@@ -17,28 +23,34 @@ print(type(d))
 
 
 
-#program for adding two numbers
+#Program-04
+
+# program for adding two numbers
 a= int(input("enter first number:"))
 b= int(input("enter second number:"))
 print("sum =", a+b)
 
 
-#program for Normal Calculation
+#Program-05
+
+# program for Normal Calculation
 a= int(input("Enter First Number:"))
 b= int(input("Enter Second Number:"))
 sum = a+b
-sub = a-b
+diff = a-b
 mul = a*b
 divide = a/b
 rem = a%b
 print(sum)     #for addition
-print(sub)     #for minus
+print(diff)     #for Substraction
 print(mul)     #for multiplication
 print(divide)  #for division
 print(rem)     #for reminder
 
 
-#program for age checker
+#Program-06
+
+# program for age checker
 x=int(input("enter your age:"))
 if(x>=18):
     print("adult")
@@ -46,7 +58,9 @@ else:
     print("not adult")
 
 
-#program for license or vote 
+#Program-07
+
+# program for license or vote 
 x= int(input("enter age:"))
 if(x>=18):
     print("can drive and can vote:")
@@ -54,20 +68,24 @@ else:
     print("can't derive and can't vote")
 
 
-#program for print area
+#Program-08
+
+# program for print area
 side= float(input("enter square side:"))  #taking input of a square side 
 print("area=", side*side)
 
 
-#program for finding average
+#Program-09
+
+# program for finding average
 a= float(input("enter first number:"))
 b= float(input("enter second number:"))
 print("avg =", (a+b)/2)
 
 
-##program for find True or False
+#Program-10
+
+# program for find True or False
 a= int(input("enter first num:"))
 b= int(input("enter second num:"))
 print(a>=b)
-
-
